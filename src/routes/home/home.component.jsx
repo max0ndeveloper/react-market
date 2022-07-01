e37debd435f836d7d37039b88526f9ef0aa76b3a
Max <maxim2003s@gmail.com>
2022-07-01 18:49:09 +0300

    Creating NavBar component and add routers

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,36 @@
+import '../../categories.styles.scss'
+import Directory from '../../components/directory/directory.component';
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "GPUs",
+      imageUrl: 'https://i.ibb.co/rfph3B2/F-20201230135856-TD1906.jpg'
+    },
+    {
+      id: 2,
+      title: "Motherboards",
+      imageUrl: 'https://i.ibb.co/rvqCDC0/MSI-MPG-Carbon-Z690-main-1024x683.jpg'
+    },
+    {
+      id: 3,
+      title: "CPUs",
+      imageUrl: 'https://i.ibb.co/zGT4bZm/4d789af8b759f3d0f424475abcfab840.png'
+    },
+    {
+      id: 4,
+      title: "RAM",
+      imageUrl: 'https://i.ibb.co/FVzZvKZ/960x0.jpg'
+    },
+    {
+      id: 5,
+      title: "Hard Drives",
+      imageUrl: 'https://i.ibb.co/KhHnYZ8/5bdc388d91f6773a179306cdda03dcd35094742cec21ba082632b2b99c60d26b.jpg'
+    }
+  ]
+  return (
+      <Directory categories={categories}/>
+  );
+}
+export default Home;
